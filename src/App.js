07595003e34a3b7f6d9fa20f3a35e4b7f08fc303
NoblePrinceii <farhan.begg@students.makeschool.com>
2021-04-01 21:35:43 -0700

    chat style 90% complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
+import Chat from './Chat';
 import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
-      {/* sidebar */}
       <Sidebar/>
       {/* chat */}
+      <Chat />
     </div>
   );
 }
